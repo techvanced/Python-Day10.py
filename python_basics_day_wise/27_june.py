67844856f773a0bc764658921eee309c9634479a
techvanced <prashant62024@gmail.com>
2025-06-26 20:30:24 +0000

    my  first push

---
+++ python_basics_day_wise/27_june.py
@@ -0,0 +1,43 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(5**3)
+print(5//6)
+print(5%3)
+
+
+a = 50
+b = 3
+
+print("The value of", a, "+", b, "is:", a + b)
+print("The value of", a, "-", b, "is:", a - b)
+print("The value of", a, "*", b, "is:", a * b)
+print("The value of", a, "/", b, "is:", a / b)
+print("The value of", a, "//", b, "is:", a // b)
+print("The value of", a, " % ", b, "is:", a + b)
+
+# a = "1"
+# b = "2"
+# print(a+b)
+# a = 1
+# b = 2
+# print(a + b)
+a = 1
+b = 2
+print(int(a) + int(b))
+# Implicit typecasting
+c = 1.9
+d = 8
+print(c + d)
+
+
+# a = input()
+# print(a)
+# a = input()
+# print("My name is", a)
+# a = input("Enter your name:")
+# print("My name is", a)
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+print(x + y) #concate
+print(int(x) + int(y))
